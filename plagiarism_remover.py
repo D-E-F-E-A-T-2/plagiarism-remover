0ffe9b15c9d5474cd43f385114cb7e49a0984f6c
Kush Choudhary <kushchoudhary8@gmail.com>
2019-03-15 15:22:03 +0530

    Add files via upload

---
+++ plagiarism_remover.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+data = input("Paste your data here: ")
+print("\n\n")
+
+for x in data:
+    print(x, end=' ')
+
+input("\n\n\n\n\nHit Enter to exit.")
